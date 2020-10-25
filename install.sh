ee05c3b67d14150d44f19d203681bd8bae17bdaf
Penn Bauman <pennbauman@protonmail.com>
2020-10-25 17:56:22 -0400

    cleaning up install

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@
 # Penn Bauman
 # pennbauman@protonmail.com
 
-if clang++ --version &> /dev/null; then
-	echo "Making (clang++)"
-	clang++ -O2 dndice.cpp -o dndice
-else
-	echo "Making (g++)"
-	g++ -O2 dndice.cpp -o dndice
-fi
+echo -n "Compiling..."
+g++ -O2 dndice.cpp -o dndice
+echo "Done"
 
 sudo mv dndice /bin/
 echo "Installed"
